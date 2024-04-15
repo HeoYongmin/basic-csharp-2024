31813c5673367b53dce3a7174577a54affce5df2
HeoYongmin <snrn2634@naver.com>
2024-04-15 09:09:26 +0900

    저번주 학습

--- day02/cs02_basic_app/ex07_collections/Program.cs
+++ day02/cs02_basic_app/ex07_collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Runtime.InteropServices;
 
 namespace ex07_collections
 {
@@ -6,23 +7,20 @@ namespace ex07_collections
     {
         static void Main(string[] args)
         {
-            int[] array = new int[5];
+            int[] array = new int[5]; // 1. 초기화
             // Console.WriteLine(int.MaxValue); // C#에 각 타입의 최대값, 최소값
             array[0] = 80;
             array[1] = 81;
             array[2] = 100;
             array[3] = 34;
             array[4] = 98;
-
-            var text = Console.ReadLine(); // 콘솔에서 값입력
-            // 여기다 변경한다
-            Console.WriteLine(text);
+            // array[5] = 100;
 
             // 2. 초기화 선언하면서 값을 바로 지정
             int[] score = new int[5] { 80, 74, 81, 90, 34 };
             // 3. 초기화 배열의 크기를 생략
-            string[] names = new string[] { "hello", "world", "c#!" };
-            // 4.초기화 그냥 다 생략
+            string[] names = new string[] { "hello", "world", "C#!" };
+            // 4. 초기화 그냥 다 생략
             float[] points = { 3.14f, 5.5f, 4.4f, 10.8f };
 
             // 타입확인
@@ -38,30 +36,29 @@ namespace ex07_collections
 
             Array.Sort(score); // 정렬
 
-            foreach (var item in names)
+            foreach (var item in score)
             {
-                Console.WriteLine($"{item}, ");
+                Console.Write($"{item}, ");
             }
-            Console.WriteLine(" ");
+            Console.WriteLine("");
 
-            Console.WriteLine(Array.BinarySearch(score, 90)); // 4 인덱스 4에 90이 존재한다.
-            Console.WriteLine(Array.IndexOf(score, 90));
+            Console.WriteLine(Array.BinarySearch(score, 90)); // 4 인덱스 4에 90이 존재한다
+            Console.WriteLine(Array.IndexOf(score, 90)); // 4 인덱스 4에 90이 존재한다
 
-            // 배열분할. C# 8.0부터 파이썬 배열 슬라이스 기능 도입
-
-            char[] array2 = new char[26]; //['Z' - 'A' +1];
+            // 배열분할. C# 8.0부터 파이썬 배열 슬라이스기능 도입
+            char[] array2 = new char[26]; // ['Z' - 'A' + 1];  90 - 65 + 1
             for (int i = 0; i < array2.Length; i++)
-                array2[i] = (char)('A' + i );
+                array2[i] = (char)('A' + i); // ABCDEF...Z
 
             foreach (var item in array2)
             {
                 Console.Write(item);
             }
-            Console.WriteLine();
+            Console.WriteLine(); // 굳이 이렇게 복잡하게 출력할 필요없음
 
-            // 배열 분할
+            // 배열 분할 전
             Console.WriteLine(array2);
-            // 배열분할 시작인덱스.. 종료인덱스+1
+            // 배열 분할 - 시작인덱스..종료인덱스+1
             Console.WriteLine(array2[..]); // System.Range
             Console.WriteLine(array2[5..]); // System.Range
             Console.WriteLine(array2[5..11]); // System.Range
@@ -76,6 +73,7 @@ namespace ex07_collections
             arrayList.Add(10);
             arrayList.Add(array2);
             arrayList.Add(true);
+            arrayList.Add("Hi");
 
             foreach (var item in arrayList)
             {
@@ -86,35 +84,46 @@ namespace ex07_collections
             arrayList.RemoveAt(1); // ArrayList에서 인덱스 1번의 값을 삭제
             arrayList.Insert(2, 25); // 2번 인덱스에 25를 추가
 
-            // Stack, Queue Python 자료구조에 배웠던 스택, 큐랑 차이없음
+            // Stack, Queue Python 자료구조에 배웠던 스택, 큐랑 차이없음!
             Stack stack = new Stack();
-             stack.Push(1);
-             stack.Pop();
+            stack.Push(1);
+            stack.Pop();
 
-             Queue que = new Queue();
-             que.Enqueue(1);
-             que.Dequeue();
+            Queue que = new Queue();
+            que.Enqueue(1);
+            que.Dequeue();
 
-            // Hasktable = Dictionary
+            // Hashtable == Dictionary
             Hashtable ht = new Hashtable();
             ht["book"] = "책";
             ht["cook"] = "요리사";
-            ht["programer"] = "프로그래머";
+            ht["programmer"] = "프로그래머";
 
-            Console.WriteLine(ht["programer"]);
+            Console.WriteLine(ht["programmer"]);
 
-            // foreach가 가능한 객체 만들기
-
+            // foreach 가 가능한 객체 만들기
+            var obj = new CustomEnumerator();
+            foreach (var item in obj)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 
     class CustomEnumerator
     {
-        int[] numbers = { 1, 2, 3, 4 };
+        int[] numbers = { 1, 2, 3, 4 }; // 임의로 마치 반복문(foreach)를 못쓴다고 가정
+
+        // foreach로 사용할 수 없는 객체를 반복문을 쓸 수있도록 만들어주는 것
         public IEnumerator GetEnumerator()
         {
-
+            // 일반 return은 return문을 만나면 메서드를 빠져나감
+            // yield return은 return문을 실행한 뒤 멈춰있음. 다음 yield return문을 실행하기 전까지
+            yield return numbers[0];
+            yield return numbers[1];
+            yield return numbers[2];
+            yield break; // 모든 로직을 빠져나감.
+            yield return numbers[3];
         }
-
     }
 }
