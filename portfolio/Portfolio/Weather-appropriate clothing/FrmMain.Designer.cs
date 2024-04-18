339f56e78f69d4e7e904858eed494a278a0caffc
HeoYongmin <snrn2634@naver.com>
2024-04-18 17:26:15 +0900

    포트폴리오 구상

---
+++ portfolio/Portfolio/Weather-appropriate clothing/FrmMain.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Weather_appropriate_clothing
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton3 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton5 = new RadioButton();
+            radioButton6 = new RadioButton();
+            radioButton7 = new RadioButton();
+            radioButton8 = new RadioButton();
+            groupBox3 = new GroupBox();
+            pictureBox1 = new PictureBox();
+            groupBox4 = new GroupBox();
+            textBox1 = new TextBox();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox4.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.FromArgb(255, 192, 128);
+            groupBox1.Font = new Font("나눔고딕코딩", 15F, FontStyle.Bold);
+            groupBox1.Location = new Point(12, 11);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(557, 41);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "기온별 옷차림";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(radioButton8);
+            groupBox2.Controls.Add(radioButton7);
+            groupBox2.Controls.Add(radioButton6);
+            groupBox2.Controls.Add(radioButton5);
+            groupBox2.Controls.Add(radioButton4);
+            groupBox2.Controls.Add(radioButton3);
+            groupBox2.Controls.Add(radioButton2);
+            groupBox2.Controls.Add(radioButton1);
+            groupBox2.Location = new Point(12, 58);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(133, 465);
+            groupBox2.TabIndex = 1;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "기온";
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(12, 33);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(79, 19);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "28도 이상";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(12, 86);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(73, 19);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "23~27도";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            radioButton3.AutoSize = true;
+            radioButton3.Location = new Point(12, 141);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(73, 19);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "20~22도";
+            radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            radioButton4.AutoSize = true;
+            radioButton4.Location = new Point(12, 198);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(73, 19);
+            radioButton4.TabIndex = 3;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "17~19도";
+            radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            radioButton5.AutoSize = true;
+            radioButton5.Location = new Point(12, 255);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(73, 19);
+            radioButton5.TabIndex = 4;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "12~16도";
+            radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            radioButton6.AutoSize = true;
+            radioButton6.Location = new Point(12, 306);
+            radioButton6.Name = "radioButton6";
+            radioButton6.Size = new Size(66, 19);
+            radioButton6.TabIndex = 5;
+            radioButton6.TabStop = true;
+            radioButton6.Text = "9~11도";
+            radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            radioButton7.AutoSize = true;
+            radioButton7.Location = new Point(12, 363);
+            radioButton7.Name = "radioButton7";
+            radioButton7.Size = new Size(59, 19);
+            radioButton7.TabIndex = 6;
+            radioButton7.TabStop = true;
+            radioButton7.Text = "5~8도";
+            radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            radioButton8.AutoSize = true;
+            radioButton8.Location = new Point(12, 416);
+            radioButton8.Name = "radioButton8";
+            radioButton8.Size = new Size(72, 19);
+            radioButton8.TabIndex = 7;
+            radioButton8.TabStop = true;
+            radioButton8.Text = "4도 이하";
+            radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(pictureBox1);
+            groupBox3.Location = new Point(151, 58);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(305, 265);
+            groupBox3.TabIndex = 2;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "옷차림";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.LightYellow;
+            pictureBox1.Location = new Point(6, 22);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(293, 237);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // groupBox4
+            // 
+            groupBox4.Controls.Add(textBox1);
+            groupBox4.Location = new Point(155, 338);
+            groupBox4.Name = "groupBox4";
+            groupBox4.Size = new Size(301, 67);
+            groupBox4.TabIndex = 3;
+            groupBox4.TabStop = false;
+            groupBox4.Text = "옷 종류";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(11, 22);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(284, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // FrmMain
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(572, 667);
+            Controls.Add(groupBox4);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox2);
+            Controls.Add(groupBox1);
+            Name = "FrmMain";
+            Text = "Form1";
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox4.ResumeLayout(false);
+            groupBox4.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private RadioButton radioButton8;
+        private RadioButton radioButton7;
+        private RadioButton radioButton6;
+        private RadioButton radioButton5;
+        private RadioButton radioButton4;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private GroupBox groupBox3;
+        private PictureBox pictureBox1;
+        private GroupBox groupBox4;
+        private TextBox textBox1;
+    }
+}
---
+++ portfolio/Portfolio/Weather-appropriate clothing/FrmMain.cs
@@ -0,0 +1,10 @@
+namespace Weather_appropriate_clothing
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+
+        }
+    }
+}
