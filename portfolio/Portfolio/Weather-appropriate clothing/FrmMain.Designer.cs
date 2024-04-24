1cef4462683f14058583a39cdad6d0402bff90a6
HeoYongmin <snrn2634@naver.com>
2024-04-24 17:28:56 +0900

    개인 포트폴리오 내일 이어서

--- portfolio/Portfolio/Weather-appropriate clothing/FrmMain.Designer.cs
+++ portfolio/Portfolio/Weather-appropriate clothing/FrmMain.Designer.cs
@@ -29,23 +29,24 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
-            groupBox2 = new GroupBox();
-            radioButton1 = new RadioButton();
-            radioButton2 = new RadioButton();
-            radioButton3 = new RadioButton();
-            radioButton4 = new RadioButton();
-            radioButton5 = new RadioButton();
-            radioButton6 = new RadioButton();
-            radioButton7 = new RadioButton();
-            radioButton8 = new RadioButton();
-            groupBox3 = new GroupBox();
-            pictureBox1 = new PictureBox();
+            weatherGroupBox = new GroupBox();
+            RdoWinter = new RadioButton();
+            RdoSummer = new RadioButton();
+            outfitGroupBox = new GroupBox();
+            PicSummerImg = new PictureBox();
             groupBox4 = new GroupBox();
-            textBox1 = new TextBox();
-            groupBox2.SuspendLayout();
-            groupBox3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            TxtDcpSummer = new TextBox();
+            groupBox5 = new GroupBox();
+            PicWinterImg = new PictureBox();
+            groupBox6 = new GroupBox();
+            TxtDcpWinter = new TextBox();
+            weatherGroupBox.SuspendLayout();
+            outfitGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)PicSummerImg).BeginInit();
             groupBox4.SuspendLayout();
+            groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)PicWinterImg).BeginInit();
+            groupBox6.SuspendLayout();
             SuspendLayout();
             // 
             // groupBox1
@@ -57,184 +58,153 @@
             groupBox1.Size = new Size(557, 41);
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
-            groupBox1.Text = "기온별 옷차림";
+            groupBox1.Text = "날씨별 옷차림";
             // 
-            // groupBox2
+            // weatherGroupBox
             // 
-            groupBox2.Controls.Add(radioButton8);
-            groupBox2.Controls.Add(radioButton7);
-            groupBox2.Controls.Add(radioButton6);
-            groupBox2.Controls.Add(radioButton5);
-            groupBox2.Controls.Add(radioButton4);
-            groupBox2.Controls.Add(radioButton3);
-            groupBox2.Controls.Add(radioButton2);
-            groupBox2.Controls.Add(radioButton1);
-            groupBox2.Location = new Point(12, 58);
-            groupBox2.Name = "groupBox2";
-            groupBox2.Size = new Size(133, 465);
-            groupBox2.TabIndex = 1;
-            groupBox2.TabStop = false;
-            groupBox2.Text = "기온";
+            weatherGroupBox.Controls.Add(RdoWinter);
+            weatherGroupBox.Controls.Add(RdoSummer);
+            weatherGroupBox.Location = new Point(12, 58);
+            weatherGroupBox.Name = "weatherGroupBox";
+            weatherGroupBox.Size = new Size(133, 512);
+            weatherGroupBox.TabIndex = 1;
+            weatherGroupBox.TabStop = false;
+            weatherGroupBox.Text = "날씨";
             // 
-            // radioButton1
+            // RdoWinter
             // 
-            radioButton1.AutoSize = true;
-            radioButton1.Location = new Point(12, 33);
-            radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(79, 19);
-            radioButton1.TabIndex = 0;
-            radioButton1.TabStop = true;
-            radioButton1.Text = "28도 이상";
-            radioButton1.UseVisualStyleBackColor = true;
+            RdoWinter.AutoSize = true;
+            RdoWinter.Location = new Point(39, 325);
+            RdoWinter.Name = "RdoWinter";
+            RdoWinter.Size = new Size(49, 19);
+            RdoWinter.TabIndex = 1;
+            RdoWinter.TabStop = true;
+            RdoWinter.Text = "겨울";
+            RdoWinter.UseVisualStyleBackColor = true;
             // 
-            // radioButton2
+            // RdoSummer
             // 
-            radioButton2.AutoSize = true;
-            radioButton2.Location = new Point(12, 86);
-            radioButton2.Name = "radioButton2";
-            radioButton2.Size = new Size(73, 19);
-            radioButton2.TabIndex = 1;
-            radioButton2.TabStop = true;
-            radioButton2.Text = "23~27도";
-            radioButton2.UseVisualStyleBackColor = true;
+            RdoSummer.AutoSize = true;
+            RdoSummer.Location = new Point(39, 62);
+            RdoSummer.Name = "RdoSummer";
+            RdoSummer.Size = new Size(49, 19);
+            RdoSummer.TabIndex = 0;
+            RdoSummer.TabStop = true;
+            RdoSummer.Text = "여름";
+            RdoSummer.UseVisualStyleBackColor = true;
+            RdoSummer.CheckedChanged += RdoSummer_CheckedChanged;
             // 
-            // radioButton3
+            // outfitGroupBox
             // 
-            radioButton3.AutoSize = true;
-            radioButton3.Location = new Point(12, 141);
-            radioButton3.Name = "radioButton3";
-            radioButton3.Size = new Size(73, 19);
-            radioButton3.TabIndex = 2;
-            radioButton3.TabStop = true;
-            radioButton3.Text = "20~22도";
-            radioButton3.UseVisualStyleBackColor = true;
+            outfitGroupBox.Controls.Add(PicSummerImg);
+            outfitGroupBox.Location = new Point(151, 58);
+            outfitGroupBox.Name = "outfitGroupBox";
+            outfitGroupBox.Size = new Size(256, 184);
+            outfitGroupBox.TabIndex = 2;
+            outfitGroupBox.TabStop = false;
+            outfitGroupBox.Text = "옷차림";
             // 
-            // radioButton4
+            // PicSummerImg
             // 
-            radioButton4.AutoSize = true;
-            radioButton4.Location = new Point(12, 198);
-            radioButton4.Name = "radioButton4";
-            radioButton4.Size = new Size(73, 19);
-            radioButton4.TabIndex = 3;
-            radioButton4.TabStop = true;
-            radioButton4.Text = "17~19도";
-            radioButton4.UseVisualStyleBackColor = true;
-            // 
-            // radioButton5
-            // 
-            radioButton5.AutoSize = true;
-            radioButton5.Location = new Point(12, 255);
-            radioButton5.Name = "radioButton5";
-            radioButton5.Size = new Size(73, 19);
-            radioButton5.TabIndex = 4;
-            radioButton5.TabStop = true;
-            radioButton5.Text = "12~16도";
-            radioButton5.UseVisualStyleBackColor = true;
-            // 
-            // radioButton6
-            // 
-            radioButton6.AutoSize = true;
-            radioButton6.Location = new Point(12, 306);
-            radioButton6.Name = "radioButton6";
-            radioButton6.Size = new Size(66, 19);
-            radioButton6.TabIndex = 5;
-            radioButton6.TabStop = true;
-            radioButton6.Text = "9~11도";
-            radioButton6.UseVisualStyleBackColor = true;
-            // 
-            // radioButton7
-            // 
-            radioButton7.AutoSize = true;
-            radioButton7.Location = new Point(12, 363);
-            radioButton7.Name = "radioButton7";
-            radioButton7.Size = new Size(59, 19);
-            radioButton7.TabIndex = 6;
-            radioButton7.TabStop = true;
-            radioButton7.Text = "5~8도";
-            radioButton7.UseVisualStyleBackColor = true;
-            // 
-            // radioButton8
-            // 
-            radioButton8.AutoSize = true;
-            radioButton8.Location = new Point(12, 416);
-            radioButton8.Name = "radioButton8";
-            radioButton8.Size = new Size(72, 19);
-            radioButton8.TabIndex = 7;
-            radioButton8.TabStop = true;
-            radioButton8.Text = "4도 이하";
-            radioButton8.UseVisualStyleBackColor = true;
-            // 
-            // groupBox3
-            // 
-            groupBox3.Controls.Add(pictureBox1);
-            groupBox3.Location = new Point(151, 58);
-            groupBox3.Name = "groupBox3";
-            groupBox3.Size = new Size(305, 265);
-            groupBox3.TabIndex = 2;
-            groupBox3.TabStop = false;
-            groupBox3.Text = "옷차림";
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.BackColor = Color.LightYellow;
-            pictureBox1.Location = new Point(6, 22);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(293, 237);
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
+            PicSummerImg.BackColor = Color.LightYellow;
+            PicSummerImg.Location = new Point(6, 15);
+            PicSummerImg.Name = "PicSummerImg";
+            PicSummerImg.Size = new Size(244, 163);
+            PicSummerImg.TabIndex = 0;
+            PicSummerImg.TabStop = false;
             // 
             // groupBox4
             // 
-            groupBox4.Controls.Add(textBox1);
-            groupBox4.Location = new Point(155, 338);
+            groupBox4.Controls.Add(TxtDcpSummer);
+            groupBox4.Location = new Point(157, 248);
             groupBox4.Name = "groupBox4";
-            groupBox4.Size = new Size(301, 67);
+            groupBox4.Size = new Size(196, 60);
             groupBox4.TabIndex = 3;
             groupBox4.TabStop = false;
             groupBox4.Text = "옷 종류";
             // 
-            // textBox1
+            // TxtDcpSummer
             // 
-            textBox1.Location = new Point(11, 22);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(284, 23);
-            textBox1.TabIndex = 0;
+            TxtDcpSummer.Location = new Point(6, 22);
+            TxtDcpSummer.Name = "TxtDcpSummer";
+            TxtDcpSummer.Size = new Size(184, 23);
+            TxtDcpSummer.TabIndex = 0;
+            // 
+            // groupBox5
+            // 
+            groupBox5.Controls.Add(PicWinterImg);
+            groupBox5.Location = new Point(151, 321);
+            groupBox5.Name = "groupBox5";
+            groupBox5.Size = new Size(256, 184);
+            groupBox5.TabIndex = 2;
+            groupBox5.TabStop = false;
+            groupBox5.Text = "옷차림";
+            // 
+            // PicWinterImg
+            // 
+            PicWinterImg.BackColor = Color.LightYellow;
+            PicWinterImg.Location = new Point(6, 15);
+            PicWinterImg.Name = "PicWinterImg";
+            PicWinterImg.Size = new Size(244, 163);
+            PicWinterImg.TabIndex = 0;
+            PicWinterImg.TabStop = false;
+            // 
+            // groupBox6
+            // 
+            groupBox6.Controls.Add(TxtDcpWinter);
+            groupBox6.Location = new Point(157, 511);
+            groupBox6.Name = "groupBox6";
+            groupBox6.Size = new Size(196, 59);
+            groupBox6.TabIndex = 3;
+            groupBox6.TabStop = false;
+            groupBox6.Text = "옷 종류";
+            // 
+            // TxtDcpWinter
+            // 
+            TxtDcpWinter.Location = new Point(6, 22);
+            TxtDcpWinter.Name = "TxtDcpWinter";
+            TxtDcpWinter.Size = new Size(184, 23);
+            TxtDcpWinter.TabIndex = 0;
             // 
             // FrmMain
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(572, 667);
+            Controls.Add(groupBox6);
             Controls.Add(groupBox4);
-            Controls.Add(groupBox3);
-            Controls.Add(groupBox2);
+            Controls.Add(groupBox5);
+            Controls.Add(outfitGroupBox);
+            Controls.Add(weatherGroupBox);
             Controls.Add(groupBox1);
             Name = "FrmMain";
             Text = "Form1";
-            groupBox2.ResumeLayout(false);
-            groupBox2.PerformLayout();
-            groupBox3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            weatherGroupBox.ResumeLayout(false);
+            weatherGroupBox.PerformLayout();
+            outfitGroupBox.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)PicSummerImg).EndInit();
             groupBox4.ResumeLayout(false);
             groupBox4.PerformLayout();
+            groupBox5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)PicWinterImg).EndInit();
+            groupBox6.ResumeLayout(false);
+            groupBox6.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private GroupBox groupBox1;
-        private GroupBox groupBox2;
-        private RadioButton radioButton8;
-        private RadioButton radioButton7;
-        private RadioButton radioButton6;
-        private RadioButton radioButton5;
-        private RadioButton radioButton4;
-        private RadioButton radioButton3;
-        private RadioButton radioButton2;
-        private RadioButton radioButton1;
-        private GroupBox groupBox3;
-        private PictureBox pictureBox1;
+        private GroupBox weatherGroupBox;
+        private GroupBox outfitGroupBox;
+        private PictureBox PicSummerImg;
         private GroupBox groupBox4;
-        private TextBox textBox1;
+        private TextBox TxtDcpSummer;
+        private RadioButton RdoWinter;
+        private RadioButton RdoSummer;
+        private GroupBox groupBox5;
+        private PictureBox PicWinterImg;
+        private GroupBox groupBox6;
+        private TextBox TxtDcpWinter;
     }
 }
--- portfolio/Portfolio/Weather-appropriate clothing/FrmMain.cs
+++ portfolio/Portfolio/Weather-appropriate clothing/FrmMain.cs
@@ -4,7 +4,23 @@ namespace Weather_appropriate_clothing
     {
         public FrmMain()
         {
+            InitializeComponent();
 
+            RadioButton RdoSummer = new RadioButton();
+            RdoSummer.Text = "여름";
+            RdoSummer.CheckedChanged += RdoSummer_CheckedChanged;
+            // RdoSummer.Location = new System.Drawing.Point(20, 20);
+        }
+
+        private void RdoSummer_CheckedChanged(object sender, EventArgs e)
+        {
+            // 여름 라디오 버튼이 선택될 때 실행되는 코드
+            if (RdoSummer.Checked)
+            {
+                // 여름 옷차림 이미지와 설명을 표시
+                PicSummerImg.Image = Properties.Resources.summer_outfit;
+                TxtDcpSummer.Text = "여름에 적합한 옷차림입니다.\n반팔 티셔츠와 반바지를 입고,\n가볍고 시원한 슬리퍼를 신으세요.";
+            }
         }
     }
 }
